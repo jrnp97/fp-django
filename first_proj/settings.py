064dac0b1d8478ad87b2bb5ce27eecf776f008ff
jnegrete <jrnp1997@gmail.com>
2022-07-18 18:46:57 -0300

    fix: include allowed hosts

--- first_proj/settings.py
+++ first_proj/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '_%#e155f8jlg=d8s4(2+ch!oo7@8*h3@009mhrxp@j5vn%b03b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'pi-fd.herokuapp.com']
 
 
 # Application definition
