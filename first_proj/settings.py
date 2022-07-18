f16c50c6f94467da098f33c95504bd52b0dcd39b
jnegrete <jrnp1997@gmail.com>
2022-07-18 18:27:22 -0300

    build: configure django-environ :hammer:

--- first_proj/settings.py
+++ first_proj/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,10 +81,7 @@ WSGI_APPLICATION = 'first_proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
