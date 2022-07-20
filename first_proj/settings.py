d12f628438a2eebe2611237ff3ae0ef428d0b995
jnegrete <jrnp1997@gmail.com>
2022-07-20 17:45:36 -0300

    doc: Add some docs

--- first_proj/settings.py
+++ first_proj/settings.py
@@ -22,6 +22,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
